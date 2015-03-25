72e2f115f56f0b6dec97cc6ce35dde0044926239
pifupro_iOS_admin <chenyl@pifupro.com>
2015-03-25 18:19:27 +0800

    Initial Commit

---
+++ CYLDifferentColorTitleButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CYLDifferentColorTitleButton
+//
+//  Created by chenyilong on 15/3/25.
+//  Copyright (c) 2015年 chenyilong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
